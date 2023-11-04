c14d1d0152982a936f4a9653d89a0546a448c373
ozawa <omy.sk.izw.ss@gmail.com>
2023-11-04 17:00:34 +0900

    ３回目

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -22,9 +22,10 @@
   @endif
 </div>
 <div class="category__content">
-  <form class="create-form">
+  <form class="create-form" action="/categories" method="post">
+    @csrf
     <div class="create-form__item">
-      <input class="create-form__item-input" type="text">
+    <input class="create-form__item-input" type="text" name="name" value="{{ old('name') }}">
     </div>
     <div class="create-form__button">
       <button class="create-form__button-submit" type="submit">作成</button>
@@ -38,9 +39,12 @@
       @foreach ($categories as $category)
       <tr class="category-table__row">
         <td class="category-table__item">
-          <form class="update-form">
+        <form class="update-form" action="/categories/update" method="post">
+          @method('PATCH')
+          @csrf
             <div class="update-form__item">
-              <input class="update-form__item-input" type="text">
+              <input class="update-form__item-input" type="text" name="name" value="{{ $category['name'] }}">
+              <input type="hidden" name="id" value="{{ $category['id'] }}">
             </div>
             <div class="update-form__button">
               <button class="update-form__button-submit" type="submit">更新</button>
@@ -48,8 +52,11 @@
           </form>
         </td>
         <td class="category-table__item">
-          <form class="delete-form">
+        <form class="delete-form" action="/categories/delete" method="post">
+          @method('DELETE')
+          @csrf
             <div class="delete-form__button">
+              <input type="hidden" name="id" value="{{ $category['id'] }}">
               <button class="delete-form__button-submit" type="submit">削除</button>
             </div>
           </form>
